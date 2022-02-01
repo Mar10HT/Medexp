cf7ec337eeffd2c05d8b766d92f44249717fa940
Mario Herrera <josetosta12@gmail.com>
2022-01-31 21:08:50 -0600

    Pruebas comprobadas

--- dao/expedientes/expedientes.model.js
+++ dao/expedientes/expedientes.model.js
@@ -6,29 +6,31 @@ class Expedientes{
         getDb()
         .then((database)=>{
             db = database;
-            if(process.env.MIGRATE === 'true'){
-                const createStatement = 'CREATE TABLE IF NOT EXITS expedientes (id INTEGER PRIMERY KEY AUTOINCREMENT, identidad TEXT, fecha TEXT, descripcion TEXT, observacion TEXT, registros TEXT, ultimaActualizacion TEXT);';
+            if (process.env.MIGRATE === 'true'){
+                const createStatement = 'CREATE TABLE IF NOT EXISTS expedientes (id INTEGER PRIMARY KEY AUTOINCREMENT, identidad TEXT, fecha TEXT, descripcion TEXT, observacion TEXT, registros TEXT, ultimaActualizacion TEXT);';
                 db.run(createStatement);
             }
         })
         .catch((err)=>{console.log(err)});
     }
 
-    new (identidad, fecha, descripcion, observacion, registros, ultimaObservacion) {
-        
-        return new Promise((accept, reject)=>{
-            db.run('INSERT INTO expedientes (identidad, fecha, descripcion, observacion, registros, ultimaObservacion) VALUES (?, ?, ?, ?, ?, ?)',
-                [identidad, fecha, descripcion, observacion, registros, ultimaObservacion],
-                (err, rslt)=>{
-                    if(err){
-                        console.error(err);
-                        reject(err);
-                    }
-                    accept(rslt);
-                }
-            );
+    new ( identidad, fecha, descripcion, observacion, registros, ultimaObservacion) {
+        return new Promise( (accept, reject)=> {
+          db.run(
+            'INSERT INTO expedientes (identidad, fecha, descripcion, observacion, registros, ultimaActualizacion) VALUES (?, ?, ?, ?, ?, ?)',
+                [identidad, fecha, descripcion, observacion, registros, ultimaObservacion]
+,
+            (err, rslt)=>{
+              if(err) {
+                console.error(err);
+                reject(err);
+              }
+              accept(rslt);
+            }
+          );
         });
-    }
+      }
+    
 
     getAll(){
         return new Promise((accept, reject)=>{
@@ -45,7 +47,7 @@ class Expedientes{
 
     getById(id){
         return new Promise((accept, reject)=>{
-            db.get('SELECT * from pacientes where id=?;', 
+            db.get('SELECT * from expedientes where id=?;', 
              [id],
              (err, row) =>{
                  if(err){
--- routes/api/v1/expedientes/expedientes.js
+++ routes/api/v1/expedientes/expedientes.js
@@ -34,16 +34,6 @@ router.get('/byid/:id', async (req, res) => {
   }
 });
 
-router.get('/byagegender/:age/:gender', async (req, res) => {
-  try {
-    const { age, gender } = req.params;
-    const row = {}; // await expedienteModel.getById(parseInt(id));
-    res.status(200).json({ status: 'ok', expedientes: row });
-  } catch (ex) {
-    console.log(ex);
-    res.status(500).json({ status: 'failed' });
-  }
-});
 
 router.post('/new', async (req, res) => {
   const {identidad, fecha, descripcion, observacion, registros, ultimaObservacion  } = req.body;
